06a085b9fe07dd61d9c344213f5b546449cd260d
xsreeex <120957359+xsreeex@users.noreply.github.com>
2023-04-12 23:22:01 +0530

    Create BizCard.py

---
+++ BizCard.py
@@ -0,0 +1,64 @@
+
+# Run the below command if you don't already have Pandas
+!pip install pandas
+!pip install snscrape
+# Imports
+
+import pandas as pd
+
+!pip install easyocr
+
+!pip install pymongo
+import pymongo as pym 
+
+
+import easyocr as ocr  #OCR
+import streamlit as st  #Web App
+import pymongo as pym 
+import pandas as pd
+from PIL import Image #Image Processing
+import numpy as np #Image Processing 
+
+#title
+st.title("BizCard - Extract Text from Business Cards")
+
+#subtitle
+st.markdown("## Optical Character Recognition - Using `easyocr`, `streamlit`")
+
+st.markdown("")
+
+#image uploader
+image = st.file_uploader(label = "Upload your image here",type=['png','jpg','jpeg'])
+
+
+
+def load_model(): 
+    reader = ocr.Reader(['en'],model_storage_directory='.')
+    return reader 
+
+reader = load_model() #load model
+input_form = st.form("input_form")
+
+if image is not None:
+
+    input_image = Image.open(image) #read image
+    st.image(input_image) #display image
+
+    with st.spinner("🤖 AI is at Work! "):
+        
+
+        result = reader.readtext(np.array(input_image))
+
+        result_text = [] #empty list for results
+        
+
+        for text in result:
+            result_text.append(text[1])
+
+        st.write(result_text)
+    #st.success("Here you go!")
+    st.balloons()
+
+else:
+    st.write("Upload an Image")
+
